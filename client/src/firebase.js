4717d78ef66acb4d5d2250a9a48fe81eb9d78cfc
MSI <hirun08072002@gmail.com>
2024-08-31 06:41:05 +0530

    my project

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzFj4b2zmrZIegk8LgQGO99ttDCUyfKnw",
+  authDomain: "fourpoint-db442.firebaseapp.com",
+  projectId: "fourpoint-db442",
+  storageBucket: "fourpoint-db442.appspot.com",
+  messagingSenderId: "744868587309",
+  appId: "1:744868587309:web:0706ca093da9f2fd4f55a4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
